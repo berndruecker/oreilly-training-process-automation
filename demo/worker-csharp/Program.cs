6d37f84192713f7a402a0f169345e18143cf235c
Bernd Ruecker <bernd.ruecker@camunda.com>
2021-07-15 08:42:11 +0200

    fixed code

--- demo/worker-csharp/Program.cs
+++ demo/worker-csharp/Program.cs
@@ -1,17 +1,26 @@
-﻿using CamundaClient;
-using System;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Extensions.Logging;
+using Zeebe.Client;
+using Zeebe.Client.Api.Responses;
+using Zeebe.Client.Api.Worker;
+using Zeebe.Client.Impl.Builder;
+using Newtonsoft.Json;
 
 namespace OReillyTraining
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] _)
         {
             IZeebeClient client = CamundaCloudClientBuilder.Builder()
                 .UseClientId("Yx6Q63H1Nx~tztL-tGo~tAqJfzLI6pkx")
                 .UseClientSecret("Qj0cv6CYTc5lmYLIlsbv7XhvPMLU--jJbf~loZywSf3UFeULrGoqtV.0-mj85q2R")
                 .UseContactPoint("9cfc0e64-b2f5-47ee-af0d-64c24341f4b7.zeebe.camunda.io:443")
-                .Build();
+                .Build();                
             
             client.NewWorker()
                 .JobType("celebrate")
@@ -22,14 +31,14 @@ namespace OReillyTraining
                 .PollingTimeout(TimeSpan.FromSeconds(30))
                 .Name("CSharpWorker")
                 .Open();
+
+            Console.Write("Type any key to stop");
+            Console.ReadLine();
         }    
 
-        void JobHandler(IJobClient jobClient, IJob activatedJob)
+        static void JobHandler(IJobClient jobClient, IJob activatedJob)
         {
-            var variables = JsonConvert.DeserializeObject<Dictionary<string, string>>(activatedJob.Variables);
-
-            Log.LogInformation("Yeah, your request was approved and can now be ordered! Please celebrate accordingly!");
-
+            Console.WriteLine("Yeah, your request was approved and can now be ordered! Please celebrate accordingly!");
             jobClient.NewCompleteJobCommand(activatedJob).Send();
         }        
     }
